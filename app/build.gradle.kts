32e9579aca95f5a9e1298f369a48e3dd0694c8d1
faresmohammedelayche <faresmohammedelayche@gmail.com>
2025-04-15 22:21:53 +0100

    FIRST EDIT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -32,11 +33,24 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Firebase BoM: لتوحيد الإصدارات
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+
+    // مكتبات Firebase بعد BoM
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    // Google Sign-In
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+
+    // اختبارات
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -1,24 +1,71 @@
 package com.example.note;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.sidesheet.SideSheetDialog;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView menu = findViewById(R.id.menu);
+        FloatingActionButton addNote = findViewById(R.id.add_note_button);
+
+        menu.setOnClickListener(view -> showSideSheet());
+        addNote.setOnClickListener(view -> goToAdd());
+    }
+
+    private void goToAdd() {
+        Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+        startActivity(intent);
+    }
+
+    private void showSideSheet() {
+        SideSheetDialog sideSheetDialog = new SideSheetDialog(this);
+        sideSheetDialog.setContentView(R.layout.side_sheet_dialog);
+        sideSheetDialog.setCanceledOnTouchOutside(true);
+        sideSheetDialog.setSheetEdge(Gravity.START);
+        sideSheetDialog.show();
+
+        sideSheetDialog.findViewById(R.id.setting).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(intent);
+            sideSheetDialog.dismiss();
+        });
+
+        sideSheetDialog.findViewById(R.id.category).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,CategoryActivity.class);
+            startActivity(intent);
+            sideSheetDialog.dismiss();
+        });
+
+        sideSheetDialog.findViewById(R.id.archives).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ArchiveActivity.class);
+            startActivity(intent);
+            sideSheetDialog.dismiss();
+        });
+
+        sideSheetDialog.findViewById(R.id.recycle).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RecycleActivity.class);
+            startActivity(intent);
+            sideSheetDialog.dismiss();
+        });
+
+        sideSheetDialog.findViewById(R.id.logout).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LogoutActivity.class);
+            startActivity(intent);
+            sideSheetDialog.dismiss();
         });
     }
 }
